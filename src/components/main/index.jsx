9cee543de5f99e0b42a3beeffcaef1cb0c908e32
phvohieunghia <phvohieunghia@gmail.com>
2022-06-03 11:20:26 +0700

    finish project

---
+++ src/components/main/index.jsx
@@ -0,0 +1,271 @@
+import React from 'react'
+
+import './style.scss'
+
+const Main = () => {
+  return (
+    <div id="main">
+      <div className="box-1 container">
+        <div className="content">
+          <h1>NGHỀ TAY TRÁI, HÁI RA TIỀN</h1>
+          <h2>NGUỒN THU NHẬP THỨ 2 TỪ KINH DOANH ONLINE</h2>
+          <ul>
+            <li>Chỉ với 4-5 tiếng/1 ngày</li>
+            <li>400+ sản phẩm nhập khẩu, chất lượng cao</li>
+            <li>Chiết khấu lên tới 30% - 55%</li>
+            <li>Đào tạo bán hàng online chuyên nghiệp từ A-Z</li>
+            <li>
+              Quản lý đơn hàng, doanh thu trên app mọi lúc mọi nơi, tiết kiệm
+              thời gian
+            </li>
+          </ul>
+        </div>
+        <div className="ball">
+          <div className="wrapper">
+            <h3>Trở thành đại lý có mức chiết khấu lên đến 30% - 55%!</h3>
+            <input type="text" replaceholder="Họ và tên" />
+            <input type="text" replaceholder="Nhập số điện thoại" />
+            <select id="">
+              <option value="">Thời gian nghe máy</option>
+              <option value="">8h30-12h</option>
+              <option value="">13h30-16h</option>
+              <option value="">Sau 16h</option>
+              <option value="">Sau 20h</option>
+            </select>
+            <input type="text" replaceholder="Email" />
+            <button>GỬI LIÊN HỆ TỚI CHÚNG TÔI</button>
+          </div>
+        </div>
+        <div className="img">
+          <div className="img-1"></div>
+        </div>
+      </div>
+      <div className="box-2">
+        <div className="row-1 container">
+          <div className="img">
+            <div className="img-1"></div>
+          </div>
+          <div className="content">
+            <h1>
+              HỢP TÁC CÙNG BCA
+              <br />
+              <span>BẠN ĐƯỢC GÌ?</span>
+            </h1>
+            <div className="text">
+              Tại BCA Solutions, chúng tôi cung cấp mọi giải pháp nhằm hỗ trợ
+              tối đa công việc kinh doanh online của bạn trở nên dễ dàng nhất có
+              thể
+            </div>
+            <div className="button">
+              <button>ĐĂNG KÝ NGAY</button>
+            </div>
+          </div>
+        </div>
+        <div className="row-2 container">
+          <div className="item col-12 col-sm-4">
+            <div className="wrapper">
+              <div className="number">01</div>
+              <div className="content">
+                <h4>KINH DOANH ONLINE 4 KHÔNG</h4>
+                <span>-Không</span>&nbsp;nhập hàng
+                <br />
+                <span>-Không</span>&nbsp;tồn kho
+                <br />
+                <span>-Không</span>&nbsp;giao hàng
+                <br />
+                <span>-Không</span>&nbsp;tự tìm kiếm khách hàng
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-4">
+            <div className="wrapper">
+              <div className="number">01</div>
+              <div className="content">
+                <h4>KINH DOANH ONLINE 4 KHÔNG</h4>
+                <span>-Không</span>&nbsp;nhập hàng
+                <br />
+                <span>-Không</span>&nbsp;tồn kho
+                <br />
+                <span>-Không</span>&nbsp;giao hàng
+                <br />
+                <span>-Không</span>&nbsp;tự tìm kiếm khách hàng
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-4">
+            <div className="wrapper">
+              <div className="number">01</div>
+              <div className="content">
+                <h4>KINH DOANH ONLINE 4 KHÔNG</h4>
+                <span>-Không</span>&nbsp;nhập hàng
+                <br />
+                <span>-Không</span>&nbsp;tồn kho
+                <br />
+                <span>-Không</span>&nbsp;giao hàng
+                <br />
+                <span>-Không</span>&nbsp;tự tìm kiếm khách hàng
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-4">
+            <div className="wrapper">
+              <div className="number">01</div>
+              <div className="content">
+                <h4>KINH DOANH ONLINE 4 KHÔNG</h4>
+                <span>-Không</span>&nbsp;nhập hàng
+                <br />
+                <span>-Không</span>&nbsp;tồn kho
+                <br />
+                <span>-Không</span>&nbsp;giao hàng
+                <br />
+                <span>-Không</span>&nbsp;tự tìm kiếm khách hàng
+              </div>
+            </div>
+          </div>
+          <div className="item col-12 col-sm-4">
+            <div className="wrapper">
+              <div className="number">01</div>
+              <div className="content">
+                <h4>KINH DOANH ONLINE 4 KHÔNG</h4>
+                <span>-Không</span>&nbsp;nhập hàng
+                <br />
+                <span>-Không</span>&nbsp;tồn kho
+                <br />
+                <span>-Không</span>&nbsp;giao hàng
+                <br />
+                <span>-Không</span>&nbsp;tự tìm kiếm khách hàng
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="box-3">
+        <div className="container">
+          <div className="content col-12 col-md-6">
+            <h2>
+              THU NHẬP TỪ
+              <br />
+              <span>20-40 TRIỆU/THÁNG</span>
+            </h2>
+            <div className="text">
+              KIẾM THÊM THU NHẬP THỨ 2,
+              <br />
+              NÂNG CAO CHẤT LƯỢNG SỐNG
+            </div>
+            <ul>
+              <li>Tận dụng thời gian rảnh gia tăng thu nhập</li>
+              <li>Không cần vất vả tìm nguồn hàng uy tín</li>
+              <li>Không cần phải nhập hàng</li>
+              <li>Mức chiết khấu cao giúp bạn có nguồn thu nhập ổn định</li>
+              <li>
+                Không cần dành nhiều thời gian đóng gói, giao hàng và quản lý
+              </li>
+              <li>Thêm thời gian, kinh tế chăm sóc bản thân và gia đình</li>
+            </ul>
+            <div className="button">
+              <button>ĐĂNG KÝ NGAY</button>
+            </div>
+          </div>
+          <div className="img col-6">
+            <div className="img-1"></div>
+          </div>
+        </div>
+      </div>
+      <div className="box-4 container">
+        <h3>
+          BCA SOLUTIONS TẠO RA
+          <br />
+          <span>HỆ SINH THÁI KINH DOANH ONLINE</span>
+          <br />
+          TOÀN DIỆN CHO MỌI NGƯỜI
+        </h3>
+        <div className="row">
+          <div className="button">
+            <button>ĐĂNG KÝ NGAY</button>
+          </div>
+          <div className="img col-12 col-md-8">
+            <div className="img-1"></div>
+          </div>
+          <div className="text col-12 col-md-4">
+            Giúp mọi người tiếp cận được
+            <span>nền tảng kinh doanh online toàn diện.</span> Giảm thiểu được
+            những công việc không sinh công. Thay vào đó là
+            <span>tập trung bán hàng, tối ưu lợi nhuận</span>
+          </div>
+        </div>
+      </div>
+      <div className="box-5">
+        <div className="container">
+          <div className="left col-5">
+            <h3>
+              <span>MS.THÁI VÂN LINH</span>
+              <br />
+              CHIA SẺ VỀ VIỆC
+              <br />
+              NÊN CÓ THÊM
+              <br />
+              <span>NGUỒN THU NHẬP</span>
+              <br />
+              <span>THỨ 2</span>
+            </h3>
+            <div className="button">
+              <button>ĐĂNG KÝ NGAY</button>
+            </div>
+          </div>
+          <div className="img col-7">
+            <div className="img-1"></div>
+          </div>
+        </div>
+      </div>
+      <div className="box-6">
+        <div className="container">
+          <h3>CẢM NHẬN CỦA ĐẠI LÝ/CTV VỀ BCA</h3>
+          <div className="button">
+            <button>ĐĂNG KÝ NGAY</button>
+          </div>
+          <div className="row">
+            <div className="item col-12 col-md-3">
+              <div className="wrapper"></div>
+            </div>
+            <div className="item col-12 col-md-3">
+              <div className="wrapper"></div>
+            </div>
+            <div className="item col-12 col-md-3">
+              <div className="wrapper"></div>
+            </div>
+            <div className="item col-12 col-md-3">
+              <div className="wrapper"></div>
+            </div>
+            <div className="item col-12 col-md-3">
+              <div className="wrapper"></div>
+            </div>
+            <div className="item col-12 col-md-3">
+              <div className="wrapper"></div>
+            </div>
+            <div className="item col-12 col-md-3">
+              <div className="wrapper"></div>
+            </div>
+            <div className="item col-12 col-md-3">
+              <div className="wrapper"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="box-7">
+        <div className="container">
+          <hr />
+          <h3>BÁO CHÍ NÓI GÌ VỀ BCA SOLUTIONS</h3>
+          <div className="button">
+            <button>ĐĂNG KÝ NGAY</button>
+          </div>
+          <div className="row">
+            <div className="img col-12 col-md-6"></div>
+            <div className="img col-12 col-md-6"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
